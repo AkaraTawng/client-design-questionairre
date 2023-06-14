4b4217b0fd6e1061e3c3ba9b0c800fc1da1cd22f
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-14 10:42:13 +0700

    add form wrapper outer, progress bar, form wrapper inner, header, body, footer, prev button, and next button styled components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,51 @@
-
+import styled from "styled-components"
 function Form() {
   return (
-    <div>Form</div>
+    <FormWrapperOuter>
+        <ProgressBar></ProgressBar>
+        <FormWrapperInner>
+        <Header></Header>
+        <Body></Body>
+        <Footer>
+            <PrevButton>Prev</PrevButton>
+            <NextButton>Next</NextButton>
+        </Footer>
+        </FormWrapperInner>
+    </FormWrapperOuter>
   )
 }
+
+const FormWrapperOuter  = styled.form`
+    
+`;
+
+const ProgressBar = styled.div`
+    
+`;
+
+const FormWrapperInner = styled.div`
+
+`;
+
+const Header = styled.header`
+    
+`;
+
+const Body = styled.main`
+    
+`;
+
+const Footer = styled.footer`
+    
+`;
+
+const PrevButton = styled.button`
+    
+`;
+
+const NextButton = styled.button`
+    
+`;
+
 
 export default Form
