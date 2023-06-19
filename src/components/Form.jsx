b9c73b3c58f4ce30f83d3bb60a63490c3bf8f696
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-19 08:10:08 +0700

    add switch statement and page display function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,26 @@
 import styled from "styled-components"
 import { useState } from "react";
+import Welcome from './Welcome';
+import PersonalInfo from './PersonalInfo';
+import BackgroundInfo from './BackgroundInfo';
+import BusinessDescription from './BusinessDescription';
+import ServicesDescription from './ServicesDescription';
+import WebsiteGoals from './WebsiteGoals';
+import ContentType from './ContentType';
+import WebsiteFeatures from './WebsiteFeatures';
+import UniqueQualities from './UniqueQualities';
+import TargetDemographic from './TargetDemographic'
+import BrandPersonality from './BrandPersonality';
+import DesignPreferences from './DesignPrefernces';
+import FavoriteWebsites from './FavoriteWebsites';
+import Submit from './Submit';
+
+
+
 function Form() {
 
   const [page, setPage] = useState(0);
-  const FormTitles = [
-    "1", 
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-  ]
+  const FormTitles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
   const handleIncrementClick = (e) => {
     // e.preventDefault();
@@ -23,6 +32,53 @@ function Form() {
     setPage((currPage) => currPage - 1)
   }
 
+  const pageDisplay = () => {
+    switch(page) {
+      case 0:
+       return <Welcome/>;
+       break;
+      case 1:
+        return <PersonalInfo/>;
+        break;
+      case 2:
+        return <BackgroundInfo/>;
+        break;
+      case 3:
+        return <BusinessDescription/>;
+        break;
+      case 4:
+        return <ServicesDescription/>;
+        break;
+      case 5:
+        return <WebsiteGoals/>;
+        break;
+      case 6:
+        return <ContentType/>;
+        break;
+      case 7:
+        return <WebsiteFeatures/>;
+        break;
+      case 8:
+        return <UniqueQualities/>;
+        break;
+      case 9:
+        return <TargetDemographic/>;
+        break;
+      case 10:
+        return <BrandPersonality/>;
+        break;
+      case 11:
+        return <DesignPreferences/>;
+        break;
+      case 12:
+        return <FavoriteWebsites/>;
+        break;
+      case 13:
+        return <Submit/>;
+        break;
+    }
+  }
+
 
 
   
@@ -31,7 +87,7 @@ function Form() {
         <ProgressBar></ProgressBar>
         <FormWrapperInner>
         <Header><h1>{FormTitles[page]}</h1></Header>
-        <Body></Body>
+        <Body>{pageDisplay()}</Body>
         <Footer>
             <PrevButton type="button" onClick={handleDecrementClick}>Back</PrevButton>
             <NextButton type="button" onClick={handleIncrementClick}>Next</NextButton>
