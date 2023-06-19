9ad36e213cf1df374bf0b6d24b300b26448b0cd5
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-19 07:25:51 +0700

    add type to button to prevent default submitting behaviour

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,16 +3,38 @@ import { useState } from "react";
 function Form() {
 
   const [page, setPage] = useState(0);
+  const FormTitles = [
+    "1", 
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+  ]
+
+  const handleIncrementClick = (e) => {
+    // e.preventDefault();
+    setPage((currPage) => currPage + 1)
+  }
+
+  const handleDecrementClick = (e) => {
+    // e.preventDefault();
+    setPage((currPage) => currPage - 1)
+  }
+
+
+
   
   return (
     <FormWrapperOuter>
         <ProgressBar></ProgressBar>
         <FormWrapperInner>
-        <Header></Header>
+        <Header><h1>{FormTitles[page]}</h1></Header>
         <Body></Body>
         <Footer>
-            <PrevButton>Prev</PrevButton>
-            <NextButton>Next</NextButton>
+            <PrevButton type="button" onClick={handleDecrementClick}>Back</PrevButton>
+            <NextButton type="button" onClick={handleIncrementClick}>Next</NextButton>
         </Footer>
         </FormWrapperInner>
     </FormWrapperOuter>
@@ -32,7 +54,9 @@ const FormWrapperInner = styled.div`
 `;
 
 const Header = styled.header`
-    
+    h1 {
+      color: red;
+    }
 `;
 
 const Body = styled.main`
