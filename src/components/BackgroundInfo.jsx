35f482f6177ef50b5408b6c47c83b9ad54b7cd84
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-22 08:34:26 +0700

    add question 2

--- src/components/BackgroundInfo.jsx
+++ src/components/BackgroundInfo.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
 function BackgroundInfo() {
   return (<>
     <Question>Do you have content ready for your site?</Question>
     <SelectionContainer>
-      <Input type='radio' id='yes' value='yes' name='background-info'/>
+      <Input type='radio' id='yes' value='yes' name='background-info-content'/>
       <Label for='yes'>Yes, I have content ready.</Label>
     </SelectionContainer>
     
     <SelectionContainer>
-      <Input type='radio' id='no' value='no' name='background-info'/>
+      <Input type='radio' id='no' value='no' name='background-info-content'/>
       <Label for='no'>Not yet. I'm still working on it.</Label>
     </SelectionContainer>
-
+  
+    <Question id='question2'>Is your company legally registered?</Question>
+    <SelectionContainer>
+      <Input type='radio' id='yes' value='yes' name='background-info-legally-registered'/>
+      <Label for='yes'>Yes.</Label>
+    </SelectionContainer>
+    
+    <SelectionContainer>
+      <Input type='radio' id='no' value='no' name='background-info-legally-registered'/>
+      <Label for='no'>No.</Label>
+    </SelectionContainer>
+   
+    <SelectionContainer>
+      <Input type='radio' id='no' value='in the process' name='background-info-legally-registered'/>
+      <Label for='no'>I'm in the process of registering it.</Label>
+    </SelectionContainer>
     </>
   )
 }
@@ -35,6 +49,10 @@ const Question = styled.p`
   padding-bottom: .5rem;
   border-bottom: 1px solid white;
   width: 80%;
+
+  &#question2 {
+    margin-top: 7rem;
+  }
 `;
 
 const SelectionContainer = styled.div`
