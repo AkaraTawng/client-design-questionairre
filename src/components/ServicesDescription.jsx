837d9cd66d98c5c6a78911bd1a0992c0b4293ca4
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-26 08:11:39 +0700

    add input

--- src/components/ServicesDescription.jsx
+++ src/components/ServicesDescription.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Question from '../utils/Question'
+import styled from 'styled-components'
+
 
 function ServicesDescription() {
-  return (
-    <div>ServicesDescription</div>
-  )
+  return (<>
+    <Question>What specific services does your company provide? List all that apply.</Question>
+    <Textarea></Textarea>
+    </>)
 }
+
+
+  const Textarea = styled.textarea`
+  width: 70%;
+  height: 8rem;
+  padding: 1rem;
+  border-radius: 20px;
+  border: none;
+  margin-top: 1.5rem;
+  &:focus {
+    border: 2px solid gray;
+  }
+`
+
 
 export default ServicesDescription
