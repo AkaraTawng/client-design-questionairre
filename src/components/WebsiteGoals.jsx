6ba7d7647d9641d2ce38d91b3b01a947eec8daaf
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-26 10:22:56 +0700

    replace textarea with reusable Textarea component. Add name attribute.

--- src/components/WebsiteGoals.jsx
+++ src/components/WebsiteGoals.jsx
@@ -1,12 +1,11 @@
 import Question from '../utils/Question'
 import TextareaInput from '../utils/Textarea'
-import styled from 'styled-components'
 
 function WebsiteGoals() {
   return (<>
     <Question>What goals do you want to accomplish with your new website?</Question>
     <TextareaInput placeholder='I.e.: Sell goods or services, disseminate knowledge, increase brand recognition, etc.
-'></TextareaInput>
+' name='website-goals'></TextareaInput>
     </>)
 }
 
