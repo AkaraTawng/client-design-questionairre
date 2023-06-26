30a06f148e493fdc765832006a4cc09af294ce24
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-26 10:31:35 +0700

    replace local textarea styled component and replace with reusable Textarea component. Add placeholder and name props to Textarea component.

--- src/components/ServicesDescription.jsx
+++ src/components/ServicesDescription.jsx
@@ -1,27 +1,16 @@
 import Question from '../utils/Question'
-import styled from 'styled-components'
-
+import Textarea from '../utils/Textarea'
 
 function ServicesDescription() {
   return (<>
     <Question>What specific services does your company provide? List all that apply.</Question>
     <Textarea placeholder='I.e.: Sell goods or services, disseminate knowledge, increase brand recognition, etc.
-'></Textarea>
+' name='services-description'></Textarea>
     </>)
 }
 
 
-  const Textarea = styled.textarea`
-  width: 70%;
-  height: 8rem;
-  padding: 1rem;
-  border-radius: 20px;
-  border: none;
-  margin-top: 1.5rem;
-  &:focus {
-    border: 2px solid gray;
-  }
-`
+
 
 
 export default ServicesDescription
