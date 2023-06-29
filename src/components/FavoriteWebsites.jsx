cf28b428f177b2b2dfa5bddad24aaec0d5edd383
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-29 09:01:59 +0700

    add question

--- src/components/FavoriteWebsites.jsx
+++ src/components/FavoriteWebsites.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Question from "../utils/Question"
 
 function FavoriteWebsites() {
-  return (
-    <div>FavoriteWebsites</div>
-  )
+  return (<>
+    <Question>
+    What are your three favorite websites and what do you like about them? If you don’t have three favorite websites, find three that you like. Please provide links.
+    </Question>
+    
+  </>)
 }
 
 export default FavoriteWebsites
