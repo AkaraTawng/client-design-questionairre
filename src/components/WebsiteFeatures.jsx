ed780090cd2824c732fabb8d6c74f6a396d46c16
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-29 08:10:37 +0700

    add checkboxes

--- src/components/WebsiteFeatures.jsx
+++ src/components/WebsiteFeatures.jsx
@@ -1,9 +1,23 @@
+import Checkbox from '../utils/Checkbox'
 import Question from '../utils/Question'
-import Textarea from '../utils/Textarea'
+
 
 function WebsiteFeatures() {
   return (<>
-    <Question>What features do you need on your website?</Question>
+    <Question>What features do you need on your website? Select all that apply.</Question>
+    <Checkbox id='payment-portal' name='payment-portal' value='payment-portal'>
+      Payment Portal
+    </Checkbox>
+    <Checkbox id='appointment-scheduling' name='appointment-scheduling' value='appointment-scheduling'>
+      Appointment Scheduling    
+    </Checkbox>
+    <Checkbox id='contact-form' name='contact-form' value='contact-form'>
+      Contact Form
+    </Checkbox>
+    <Checkbox id='other' name='other' value='other' placeholder='Please list all that apply.'>
+      Other
+    </Checkbox>
+
   </>)
 }
 
