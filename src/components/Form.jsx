5d88f2d17a38506361c20d31588c9e48649d3ffc
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-30 08:53:49 +0700

    set form variable to useRef

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -26,6 +26,7 @@ function Form() {
 
   const [page, setPage] = useState(0);
   const FormTitles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  const form = useRef();
 
   const handleIncrementClick = (e) => {
     // e.preventDefault();
