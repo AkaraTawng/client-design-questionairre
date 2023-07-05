ae14868af62ddf97de726124ce066d9e84eb889f
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-05 09:15:42 +0700

    Update BrandPersonality name attribute

--- src/components/BrandPersonality.jsx
+++ src/components/BrandPersonality.jsx
@@ -4,7 +4,7 @@ import Textarea from "../utils/Textarea"
 function BrandPersonality() {
   return (<>
     <Question>What is your brand's personality and how do you want it reflected on your website?</Question>
-    <Textarea placeholder={'Please explain.'} name=''></Textarea>
+    <Textarea placeholder={'Please explain.'} name='user_brandPersonality'></Textarea>
   </>)
 }
 
