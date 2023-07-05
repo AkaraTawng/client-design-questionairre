8161d3bc5af3445ec44d459801647a7a47142b1c
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-05 09:17:38 +0700

    Update DesignPreferences name and value attributes

--- src/components/DesignPrefernces.jsx
+++ src/components/DesignPrefernces.jsx
@@ -4,10 +4,10 @@ import RadioButton from "../utils/RadioButton"
 function DesignPrefernces() {
   return (<>
     <Question>Do you have any specific design prefernces that should be followed?</Question>
-    <RadioButton id='no-design-preferences' value='no-design-preferences' name='design-preferences'>
+    <RadioButton id='no-design-preferences' value='no' name='user_designPreferences'>
       No.
     </RadioButton>
-    <RadioButton id='yes-design-preferences' value='yes-design-preferences' name='design-preferences'>
+    <RadioButton id='yes-design-preferences' value='yes' name='user_designPreferences'>
       Yes.
     </RadioButton>
 
