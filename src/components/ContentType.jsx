4ae703a4607f77d917c26e7d06fe20f57f549161
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-05 09:09:24 +0700

    Update ContentType name attribute

--- src/components/ContentType.jsx
+++ src/components/ContentType.jsx
@@ -4,22 +4,22 @@ import Checkbox from "../utils/Checkbox"
 function ContentType() {
   return (<>
     <Question>What kind of content do you plan to publish on your website? Select all that apply. </Question>
-    <Checkbox id='picture' name='picture' value='picture'>
+    <Checkbox id='picture' name='user_contentType' value='picture'>
       Pictures
     </Checkbox>
-    <Checkbox id='video' name='video' value='video'>
+    <Checkbox id='video' name='user_contentType' value='video'>
       Videos
     </Checkbox>
-    <Checkbox id='music-files' name='music-files' value='music-files'>
+    <Checkbox id='music-files' name='user_contentType' value='music files'>
       Music files
     </Checkbox>
-    <Checkbox id='blog-posts' name='blog-posts' value='blog-posts'>
+    <Checkbox id='blog-posts' name='user_contentType' value='blog posts'>
       Blog posts
     </Checkbox>
-    <Checkbox id='product-listings' name='product-listings' value='product-listings'>
+    <Checkbox id='product-listings' name='user_contentType' value='product listings'>
       Product listings
     </Checkbox>
-    <Checkbox id='other' name='other' value='other' placeholder='Please explain.'>
+    <Checkbox id='other' name='user_contentType' value='other' placeholder='Please explain.'>
       Other
     </Checkbox>
   </>)
