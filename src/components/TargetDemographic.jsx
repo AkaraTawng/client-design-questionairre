10de35228797d0eb7b9db2c380b1800625ec87cf
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-05 09:14:42 +0700

    Update TargetDemographic name attribute

--- src/components/TargetDemographic.jsx
+++ src/components/TargetDemographic.jsx
@@ -4,7 +4,7 @@ import Textarea from "../utils/Textarea"
 function TargetDemographic() {
   return (<>
       <Question>What is your target demographic?</Question>
-      <Textarea placeholder={'Please explain.'} name=''></Textarea>
+      <Textarea placeholder={'Please explain.'} name='user_targetDemographic'></Textarea>
     </>
   )
 }
