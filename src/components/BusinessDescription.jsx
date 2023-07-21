55a5a03807528e685910245d78381209c46d06b0
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-21 08:24:48 +0700

    remove business-description id. add global bottom margin to Textarea

--- src/components/BusinessDescription.jsx
+++ src/components/BusinessDescription.jsx
@@ -4,7 +4,7 @@ import Textarea from '../utils/Textarea'
 function BusinessDescription() {
   return (<>
     <Question>Describe your business in a few sentences.</Question>
-    <Textarea id='business-description' name="user_businessDescription"></Textarea>
+    <Textarea name="user_businessDescription"></Textarea>
   </>)
 }
 
