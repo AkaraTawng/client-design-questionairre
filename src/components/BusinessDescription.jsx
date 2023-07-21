6fa9c080d185e902da0af195ad20069345fa048e
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-22 06:55:32 +0700

    add BusinessDescription placeholder text. increase Textarea component's placeholder text font size

--- src/components/BusinessDescription.jsx
+++ src/components/BusinessDescription.jsx
@@ -4,7 +4,7 @@ import Textarea from '../utils/Textarea'
 function BusinessDescription() {
   return (<>
     <Question>Describe your business in a few sentences.</Question>
-    <Textarea name="user_businessDescription"></Textarea>
+    <Textarea placeholder='Please describe your business here.' name="user_businessDescription"></Textarea>
   </>)
 }
 
