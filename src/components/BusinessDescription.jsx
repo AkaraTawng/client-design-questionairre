7cdda76fa5b865f10380609a36da9a9d23c8d03c
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-21 08:18:26 +0700

    increase Textarea TextareaInput width and height. add margin bottom to textarea in BusinessDescription

--- src/components/BusinessDescription.jsx
+++ src/components/BusinessDescription.jsx
@@ -1,9 +1,10 @@
+import styled from 'styled-components'
 import Question from '../utils/Question'
 import Textarea from '../utils/Textarea'
 function BusinessDescription() {
   return (<>
     <Question>Describe your business in a few sentences.</Question>
-    <Textarea name="user_businessDescription"></Textarea>
+    <Textarea id='business-description' name="user_businessDescription"></Textarea>
   </>)
 }
 
