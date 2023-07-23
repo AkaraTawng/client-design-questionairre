97ef4565f0b26194730ebd23d5a1047d84f35533
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-24 06:31:11 +0700

    increase Question width to 100%

--- src/components/TargetDemographic.jsx
+++ src/components/TargetDemographic.jsx
@@ -3,8 +3,8 @@ import Textarea from "../utils/Textarea"
 
 function TargetDemographic() {
   return (<>
-      <Question>What is your target demographic?</Question>
-      <Textarea placeholder={'Please explain.'} name='user_targetDemographic'></Textarea>
+      <Question className={'target-demographic'}>What is your target demographic?</Question>
+      <Textarea className={'target-demographic'} placeholder={'Please explain.'} name='user_targetDemographic'></Textarea>
     </>
   )
 }
