89001a5c228ec3c535cd186ce7496a3d1b02130a
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-24 07:27:50 +0700

    move website goals content closer to top of viewport

--- src/components/WebsiteGoals.jsx
+++ src/components/WebsiteGoals.jsx
@@ -3,7 +3,7 @@ import TextareaInput from '../utils/Textarea'
 
 function WebsiteGoals() {
   return (<>
-    <Question>What goals do you want to accomplish with your new website?</Question>
+    <Question className={'website-goals'}>What goals do you want to accomplish with your new website?</Question>
     <TextareaInput placeholder='i.e.; Sell goods or services, disseminate knowledge, increase brand recognition, etc.
 ' name='user_websiteGoals'></TextareaInput>
     </>)
