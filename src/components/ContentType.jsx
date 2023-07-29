66c10f04789b32b418330a2ac922e5fde8e20f3d
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-07-30 06:12:04 +0700

    add margin top to content type question

--- src/components/ContentType.jsx
+++ src/components/ContentType.jsx
@@ -41,7 +41,7 @@ function ContentType() {
 //  console.log(contentTypes)
 
   return (<>
-    <Question>What kind of content do you plan to publish on your website? Select all that apply. </Question>
+    <Question className={'content-type'}>What kind of content do you plan to publish on your website? Select all that apply. </Question>
     
     {contentTypes.map((contentType, index) => (
         <Checkbox
