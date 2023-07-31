c0baa8843fc405a0951cc5180c7fce8146146ced
Muhammad Fitrah Munir <fitrahfm@Muhammads-MacBook-Pro.local>
2023-07-31 12:14:05 +0800

    fix(FavoriteWebsites): add newline html entity to TextArea placeholder

--- src/components/FavoriteWebsites.jsx
+++ src/components/FavoriteWebsites.jsx
@@ -1,18 +1,21 @@
-import Question from "../utils/Question"
-import Textarea from "../utils/Textarea"
+import Question from "../utils/Question";
+import Textarea from "../utils/Textarea";
 
 function FavoriteWebsites() {
-  return (<>
-    <Question className={'favorite-websites'}>
-    What are your three favorite websites and what do you like about them? If you don’t have three favorite websites, find three that you like. Please provide links.
-    </Question>
-    <Textarea name='user_favoriteWebsites' id='favsites' 
-    placeholder='
-      websitelink1@example.com
-      like 1, like 2, like 3, ...'>
-      </Textarea>
-
-  </>)
+  return (
+    <>
+      <Question className={"favorite-websites"}>
+        What are your three favorite websites and what do you like about them?
+        If you don’t have three favorite websites, find three that you like.
+        Please provide links.
+      </Question>
+      <Textarea
+        name="user_favoriteWebsites"
+        id="favsites"
+        placeholder="websitelink1@example.com&#10;like 1, like 2, like 3, ..."
+      ></Textarea>
+    </>
+  );
 }
 
-export default FavoriteWebsites
+export default FavoriteWebsites;
