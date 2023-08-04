a90bfd3aeaac8c6c0e28b714fa6f2408f9880f7b
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-08-04 13:32:06 +0700

    adjust FavoriteWebsites placeholder text position

--- src/components/FavoriteWebsites.jsx
+++ src/components/FavoriteWebsites.jsx
@@ -7,9 +7,7 @@ function FavoriteWebsites() {
     What are your three favorite websites and what do you like about them? If you don’t have three favorite websites, find three that you like. Please provide links.
     </Question>
     <Textarea name='user_favoriteWebsites' id='favsites' 
-    placeholder='
-      websitelink1@example.com
-      like 1, like 2, like 3, ...'>
+    placeholder={`websitelink1@example.com \nlike 1, like 2, like 3, ...`}>
       </Textarea>
 
   </>)
